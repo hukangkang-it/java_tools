445b61c5983a2994e3c0a5ca459dea93cb2d593d
hukangkang <hukangkang-it@bestpay.com.cn>
2019-12-26 16:26:52 +0800

    初始化项目

---
+++ src/main/java/com/hkk/spring_tools/SpringToolsApplication.java
@@ -0,0 +1,13 @@
+package com.hkk.spring_tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringToolsApplication.class, args);
+    }
+
+}
