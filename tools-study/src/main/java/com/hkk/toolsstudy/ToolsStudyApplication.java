58a63dd0fa7560b8a7a8dad8a541cb2164533d95
hukangkang <hukangkang-it@bestpay.com.cn>
2019-12-29 14:08:07 +0800

    添加子Module

---
+++ tools-study/src/main/java/com/hkk/toolsstudy/ToolsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.hkk.toolsstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolsStudyApplication.class, args);
+    }
+
+}
