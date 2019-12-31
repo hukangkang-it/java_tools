6c031a6de960dce5104b2cc48ee87881fd867c29
hukangkang <hukangkang-it@bestpay.com.cn>
2019-12-31 16:51:16 +0800

    添加依赖

--- tools-web/src/test/java/com/hkk/toolsweb/SpringBootTestBaseInfo.java
+++ tools-web/src/test/java/com/hkk/toolsweb/SpringBootTestBaseInfo.java
@@ -1,7 +1,6 @@
 package com.hkk.toolsweb;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 
 @SpringBootTest
